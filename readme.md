54f45c74ba5dc6698c8a6a028fb229999fac1f5a
devjanaprime <dev@primeacademy.io>
2016-08-05 16:16:33 -0500

    updated readme

---
+++ readme.md
@@ -0,0 +1,12 @@
+Vanilla JS Dice Roller
+======================
+
+A simple vanilla Javascript experiment that (like my JC Deck Shuffler, https://github.com/devjanaprime/deckShuffler) I'll do this also in JS, JQuery, and Angular.
+Simulates rolling a die of the following sides:
+
+* 4
+* 6
+* 8
+* 10
+* 12
+* 20
--- scripts/diceRoller.js
+++ scripts/diceRoller.js
@@ -1,14 +1,6 @@
 console.log( 'js sourced' );
 // array to hold the history of rolls
 var rolls =[];
-// roll die function
-var rollDie = function( maxVal ){
-  console.log( 'in rollDie:', maxVal );
-  // math.random gives a value btwn 0 and 1
-  // multiply that by maxVal to get a number in the given range
-  // math.floor rounds down to nearest integer
-  return Math.floor( Math.random() * maxVal ) + 1;
-}
 // make roll function
 var makeRoll = function(){
   // get user input from select
@@ -22,6 +14,29 @@ var makeRoll = function(){
   }
   // push it into the rolls array
   rolls.push( newRoll );
-  // log it out
-  console.log( rolls );
-}
+  // display rolls
+  showRolls();
+}; // end funk
+// roll die function
+var rollDie = function( maxVal ){
+  console.log( 'in rollDie:', maxVal );
+  // math.random gives a value btwn 0 and 1
+  // multiply that by maxVal to get a number in the given range
+  // math.floor rounds down to nearest integer
+  return Math.floor( Math.random() * maxVal ) + 1;
+}; // end funk
+// display rolls to DOM
+var showRolls = function(){
+  // clear div
+  document.getElementById( 'outputDiv' ).innerHTML = "";
+  // unordered list for the cards
+  var outputList = document.createElement( 'ul' );
+  // for each card add a list item
+  for( var i=0; i < rolls.length; i++ ){
+    var listItem = document.createElement( 'li' );
+    var textnode = document.createTextNode( 'roll ' + i + ': ' + rolls[ i ].roll + ' out of ' + rolls[ i ].max );
+    listItem.appendChild( textnode );
+    outputList.appendChild( listItem );
+  }
+  document.getElementById( 'outputDiv' ).appendChild( outputList );
+}; // end funk
